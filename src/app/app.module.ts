76ee2460b66d929e4c4d38bf491dfe8e48b5b55a
cre8t <1danielmroz@gmail.com>
2019-10-10 17:39:04 +0200

    Adding boostrap, and dashboard component + front page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { ProductTileViewBoxComponent } from './product-tile-view-box/product-tile-view-box.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductViewComponent,
-    ProductTileViewBoxComponent
+    ProductTileViewBoxComponent,
+    DashboardComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
